7aea6c6d0673615f3c9f95c4f74039b2cfd4eec7
JenCastioni <jennifer.castioni@gmail.com>
2020-08-26 16:43:48 -0400

    Added "s" to items in update curl script

--- app/routes/item_routes.js
+++ app/routes/item_routes.js
@@ -75,7 +75,7 @@ router.post('/items', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/items/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/items/:id/update', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
   delete req.body.item.owner
